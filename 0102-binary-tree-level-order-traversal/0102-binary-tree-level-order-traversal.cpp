75bead1edad378aa99a2d5fc319d49299b6e2bd7
Kundan Kumar <149096505+kkundantech@users.noreply.github.com>
2026-06-26 14:44:42 +0530

    Time: 3 ms (26.09%), Space: 17.1 MB (44.07%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -12,30 +12,63 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-    vector<vector<int>> ans;
-    if(root == nullptr) return ans;
+    // vector<vector<int>> ans; //  for store the ans.
+    // if(root == nullptr) return ans;
 
-    queue<TreeNode*> q;
-    q.push(root);
+    // queue<TreeNode*> q;
+    // q.push(root); //  first push the root node
 
-    while(!q.empty()){
-        int size = q.size();        // nodes in current level
-        vector<int> level;
+    // while(!q.empty()){
+    //     int size = q.size();        // nodes in current level
+    //     vector<int> level;
 
-        for(int i = 0; i < size; i++){
-            TreeNode* first = q.front();
-            q.pop();
+    //     for(int i = 0; i < size; i++){
+    //         TreeNode* first = q.front(); // take the front of queue 
+    //         q.pop(); // then delete it 
 
-            level.push_back(first->val);
+    //         level.push_back(first->val); //  and push to the level ans 
 
-            if(first->left)  q.push(first->left);
-            if(first->right) q.push(first->right);
-        }
+    //         if(first->left)  q.push(first->left); //  then next push the left of the root 
+    //         if(first->right) q.push(first->right); //  then push the right of the root 
+    //     }
 
-        ans.push_back(level);
+    //     ans.push_back(level);
+    // }
+
+    // return ans;
+
+
+    // lets revise the same concept
+
+    vector<vector<int>>ans;
+    if(root == NULL){
+        return ans;
     }
+        queue<TreeNode*>q;
+        q.push(root);
 
-    return ans;
+        while(!q.empty()){
+            int size = q.size();
+            vector<int>level;
+
+            for(int i = 0 ; i < size ; i++){
+                TreeNode* currNode = q.front();
+                q.pop();
+
+                level.push_back(currNode->val);
+
+                if(currNode->left){
+                    q.push(currNode->left);
+                }
+
+                if(currNode->right){
+                    q.push(currNode->right);
+                }
+            }
+            ans.push_back(level);
+        }
+        return ans;
+   
 }
 
 };
