2fd0dbe15577dda032a05766b379b7fd872421aa
Kundan Kumar <149096505+kkundantech@users.noreply.github.com>
2026-09-04 16:22:20 +0530

    Time: 0 ms (100%), Space: 24 MB (7.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -12,17 +12,25 @@ public:
     //     nums[i]=0;
     //   }
 
+    // int j = 0;
+    // for(int i = 0 ; i < nums.size() ; i++){
+    //     if(nums[i] != 0){
+    //         nums[j] = nums[i];
+    //         j++;
+    //     }
+       
+    // }
+
+    // for(int i = j ; i < nums.size() ; i++){
+    //     nums[i] = 0;
+    // }
+
     int j = 0;
     for(int i = 0 ; i < nums.size() ; i++){
         if(nums[i] != 0){
-            nums[j] = nums[i];
+            swap(nums[i],nums[j]);
             j++;
         }
-       
-    }
-
-    for(int i = j ; i < nums.size() ; i++){
-        nums[i] = 0;
     }
    
 
