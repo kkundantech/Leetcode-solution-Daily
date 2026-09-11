5ed7ca04f1def935da4c8896037e8a458fcf572c
Kundan Kumar <149096505+kkundantech@users.noreply.github.com>
2026-09-11 10:38:47 +0530

    Time: 0 ms (100%), Space: 263.5 MB (16.56%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // brute force approach 
+        // int n = nums.size() - 1;
+        // while(k--){
+        //      int temp = nums[n];
+        //     for(int i = nums.size()-2 ; i >= 0 ; i--){
+        //         nums[i+1] = nums[i];
+        //     }
+        //     nums[0] = temp;
+        // }
+       // optimal sotition 
+       int n = nums.size();
+       k = k % n;
+       reverse(nums.begin() , nums.end());
+       reverse(nums.begin(), nums.begin()+k);
+       reverse(nums.begin()+k , nums.end());
+    }
+};
