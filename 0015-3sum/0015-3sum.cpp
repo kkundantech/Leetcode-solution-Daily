d53f5960991a3515d206dbdf4385850bbf08030a
Kundan Kumar <149096505+kkundantech@users.noreply.github.com>
2026-09-17 17:51:17 +0530

    Time: 50 ms (44.78%), Space: 29.2 MB (46.01%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -7,7 +7,7 @@ public:
 
         int n = nums.size();
 
-        for(int i = 0 ; i < n ; i++){
+        for(int i = 0 ; i <= n-3 ; i++){
             if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
