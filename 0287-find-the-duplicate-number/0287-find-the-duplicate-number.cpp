af004191d05f2f04545aa7536cdc132fe5957136
Kundan Kumar <149096505+kkundantech@users.noreply.github.com>
2026-09-18 11:08:54 +0530

    Time: 0 ms (100%), Space: 65 MB (90.73%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -22,16 +22,28 @@ public:
         // }
         // return 0;
 
-        // using map O{n}
+        // using map O{n} SC = O{n}
 
-        unordered_map<int, int>mp;
-        for(int i = 0 ; i < n ; i++){
-            if(mp.find(nums[i]) != mp.end()){
-                return nums[i];
+        // unordered_map<int, int>mp;
+        // for(int i = 0 ; i < n ; i++){
+        //     if(mp.find(nums[i]) != mp.end()){
+        //         return nums[i];
+        //     }
+        //     mp[nums[i]]++;
+        // }
+        // return 0;
+
+        // optamize space complexcity as well
+        for(int i = 0; i<n ; i++){
+            int index = abs(nums[i]);
+            if(nums[index - 1 ] < 0){ // see why index - 1 because nums is zero index array 
+                return index;         // here if same index will find in negative then return it
             }
-            mp[nums[i]]++;
+            nums[index - 1 ] *= -1; //  marking negative the index
         }
-        return 0;
+        return -1;
+
+
 
     }
 };
