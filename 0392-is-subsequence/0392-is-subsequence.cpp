a2b01b7edb5f78a3a86aa9745064b701c8e00956
Kundan Kumar <149096505+kkundantech@users.noreply.github.com>
2026-09-19 13:21:15 +0530

    Time: 0 ms (100%), Space: 9.4 MB (5.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -2,19 +2,44 @@ class Solution {
 public:
     bool isSubsequence(string s, string t) {
         
-        int n = s.length();
-        int m = t.length();
+        int m = s.length();
+        int n = t.length();
 
-        int i = 0;
-        int j = 0;
-        while(i < n && j < m){
-            if(s[i] == t[j]){
-                i++;
-            }
-            j++;
+        // int i = 0;
+        // int j = 0;
+        // while(i < n && j < m){
+        //     if(s[i] == t[j]){
+        //         i++;
+        //     }
+        //     j++;
+        // }
+
+        // return i == n;
+
+        // follow up
+
+        map<char,vector<int>>mp;
+        for(int i = 0 ; i < n ; i++){
+            mp[t[i]].push_back(i);
         }
 
-        return i == n;
+        int prev = -1;
+        for(int i = 0 ; i < m ; i++){
+            char ch = s[i];
+            if(mp.find(ch) == mp.end()){
+                return false;
+            }
+
+            vector<int>indices = mp[ch];
+         auto it = upper_bound(begin(indices) , end(indices) , prev);
+         if(it == indices.end()){
+            return false;
+         }
+
+         prev = *it;
+
+        }
+        return true;
 
 
     }
